9f091d80ad409fe6ce4bb242c6112f947f1aa6a0
borisgalicky <boris.galicky@akademiasovy.sk>
2018-04-05 13:30:49 +0200

    created basic of game logic (shuffle, mouseclick counter, stuff about buttons

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,64 +5,134 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
-
-
+import java.util.Random;
 import java.util.Optional;
 
 
 public class Controller{
     @FXML
-    private Button newgame_btn;
+    public Button newgame_btn;
     @FXML
-    private Button exitgame_btn;
+    public Button exitgame_btn;
+    @FXML
+    public ImageView img1;
+    @FXML
+    public ImageView img2;
+    @FXML
+    public ImageView img3;
+    @FXML
+    public ImageView img4;
+    @FXML
+    public ImageView img5;
+    @FXML
+    public ImageView img6;
+    @FXML
+    public ImageView img7;
+    @FXML
+    public ImageView img8;
+    @FXML
+    public ImageView img9;
+    @FXML
+    public ImageView img10;
+    @FXML
+    public ImageView img11;
+    @FXML
+    public ImageView img12;
+    @FXML
+    public ImageView img13;
+    @FXML
+    public ImageView img14;
+    @FXML
+    public ImageView img15;
+    @FXML
+    public ImageView img16;
     public boolean inProgress=false;
+    public int[][]arr=new int[4][4];
+    public int clicks=0;
 
     public void startGame(){
         if(inProgress==true){
-            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            Alert alert=new Alert(Alert.AlertType.CONFIRMATION);
             alert.setTitle("Restart game");
             alert.setHeaderText("You're going to restart game.");
             alert.setContentText("All your game progress will be lost. Are you sure?");
             Optional<ButtonType> result = alert.showAndWait();
-            if ((result.get() == ButtonType.OK) && inProgress == true) {
-                System.out.println("Game restarted!");
-                newgame_btn.setText("New game");
-            } else {
-                System.out.println("Game not restarted!");
+            if((result.get()==ButtonType.OK)&&inProgress == true){
+                newgame_btn.setDisable(true);
+                clicks=0;
+                Random rn=new Random();
+                for(int i=0;i<4;i++){
+                    for(int j=0;j<4;j++){
+                        int answer=rn.nextInt(2)+0;
+                        arr[i][j]=answer;
+                    }
+                }
+                for(int i=0;i<4;i++){
+                    for(int j=0;j<4;j++){
+                        System.out.print(arr[i][j]+"  ");
+                    }
+                    System.out.println("\n");
+                }
+            }else{
+                //nothing
             }
         }
         else if(inProgress==false){
+            Random rn=new Random();
+            for(int i=0;i<4;i++){
+                for(int j=0;j<4;j++){
+                    int answer=rn.nextInt(2)+0;
+                    arr[i][j]=answer;
+                }
+            }
+            for(int i=0;i<4;i++){
+                for(int j=0;j<4;j++){
+                    System.out.print(arr[i][j]+"  ");
+                }
+                System.out.println("\n");
+            }
             newgame_btn.setDisable(true);
             newgame_btn.setText("Restart");
-            System.out.println("Game started!");
             inProgress=true;
         }
     }
-    public void exitGame(){
+    public void processGame(javafx.scene.input.MouseEvent mouseEvent){
         if(inProgress==true) {
-            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            if (mouseEvent.getSource() == img1) {
+                newgame_btn.setDisable(false);
+                System.out.println("Clicked!");
+                clicks++;
+            }
+        }
+    }
+    public void exitGame(){
+        if(inProgress==true){
+            Alert alert=new Alert(Alert.AlertType.CONFIRMATION);
             alert.setTitle("Exit game");
             alert.setHeaderText("You're going to leave game.");
             alert.setContentText("All your game progress will be lost. Are you sure?");
             Optional<ButtonType> result = alert.showAndWait();
-            if ((result.get() == ButtonType.OK) && inProgress == true) {
+            if((result.get()==ButtonType.OK)&&inProgress == true){
                 Platform.exit();
-            } else {
-                // ... user chose CANCEL or closed the dialog
+            }else{
+                //nothing
             }
         }
         else{
-            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            Alert alert=new Alert(Alert.AlertType.CONFIRMATION);
             alert.setTitle("Exit game");
             alert.setHeaderText("You're going to leave game.");
             alert.setContentText("Are you sure?");
             Optional<ButtonType> result = alert.showAndWait();
-            if ((result.get() == ButtonType.OK)) {
+            if((result.get()==ButtonType.OK)){
                 Platform.exit();
-            } else {
-                // ... user chose CANCEL or closed the dialog
+            }else{
+                //nothing
             }
         }
     }
+
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,6 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("XO Game");
+        primaryStage.setResizable(false);
         primaryStage.setScene(new Scene(root,517 ,362));
         primaryStage.show();
     }
